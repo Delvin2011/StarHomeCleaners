181139e5a1a0a90021b53a454a477bdc929ce4b2
Ben Tapfuma <takutapfu@gmail.com>
2020-07-08 00:25:27 +0200

    Adding Facebook messenger

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,14 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.8.0";
 import CleaningPage from "views/CleaningPage/CleaningPage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
+  
   <Router history={hist}>
+    <MessengerCustomerChat pageId="100313711439727" appId="" size="small"/>
     <Switch>
       <Route path="/login-page" component={LoginPage} />
       <Route path="/" component={CleaningPage} />
